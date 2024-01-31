4c8fcf0025987315baa31d9be583f27d2ca85d21
Yazid Al Ghozali <yazid.al2418@gmail.com>
2024-01-31 23:00:41 +0700

    Initial Commit

---
+++ GoPrivate/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GoPrivate
+//
+//  Created by Yazid Al Ghozali on 31/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GoPrivate/GoPrivateApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoPrivateApp.swift
+//  GoPrivate
+//
+//  Created by Yazid Al Ghozali on 31/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GoPrivateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
